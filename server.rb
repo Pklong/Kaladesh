cbaab329da396ab0840d8bdba7629b42ddc0e6e8
PKLong <patrickklong@gmail.com>
2016-10-27 21:09:13 -0400

    Add titles to erb view.

--- server.rb
+++ server.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
-require 'byebug'
+require 'sinatra/reloader' if development?
 require 'json'
 
 get '/api/articles' do
-  file = JSON.parse(File.read("./data/articles.json"))
-  titles = file.map { |x| "\\n" + x["title"] }
+  file = JSON.parse(File.read("./views/articles.json"))
+  @titles = file.map { |x| x["title"] }
+
   erb :index
 end
