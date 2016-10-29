dc3b9fcac1c2f6d32a9914e4aeff062b867c95ee
PKLong <patrickklong@gmail.com>
2016-10-28 22:35:36 -0400

    Add tags.

--- server.rb
+++ server.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
+require 'byebug'
 
 get '/articles' do
   file = JSON.parse(File.read("./views/articles.json"))
   @titles = file.map { |x| [x["url"], x["title"]] }
-
+  @tags = file.map do |x|
+    x["tags"].map { | y| y["name"] }
+  end
+  # byebug
   erb :index
 end
