38f276a0dbdb8b8692b5a0eecf5670b449cc8338
PKLong <patrickklong@gmail.com>
2016-10-30 22:29:15 -0400

    Add skeleton infinite scroll.

--- server.rb
+++ server.rb
@@ -1,14 +1,39 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sinatra/json'
 require 'json'
 require 'byebug'
 
+ARTICLES = "./views/articles.json"
+OVERFLOW_ARTICLES = "./views/more-articles.json"
+
+get '/' do
+
+end
+
 get '/articles' do
-  file = JSON.parse(File.read("./views/articles.json"))
-  @titles = file.map { |x| [x["url"], x["title"]] }
+  page_view = params["page"] || 1
+  file ||= JSON.parse(File.read(ARTICLES))
+
+  articles(file)
+
   @tags = file.map do |x|
     x["tags"].map { | y| y["name"] }
   end
-  # byebug
   erb :index
 end
+
+get '/api' do
+  json JSON.parse(File.read(OVERFLOW_ARTICLES))
+end
+
+
+private
+  def articles(p)
+    @titles = []
+    10.times do |x|
+      title = p.shift()
+      @titles << [title["url"], title["title"]]
+    end
+    @titles
+  end
